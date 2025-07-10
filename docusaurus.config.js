216980011b7b4a5edd363762ce3d47e0a40623a8
GJ Twinkle Sijo <twinkle.sijo@dicortech.com>
2025-07-10 16:36:48 +0530

    Fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/test/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: '2winkle95', // Usually your GitHub org/user name.
+  projectName: 'test', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
